2c93cc29e20465f8a084ab86b4546874a57a9c99
JeetDaiya <202301017@dau.ac.in>
2026-07-20 22:48:24 +0530

    Remove unused code, shift from scattered env loading to BaseSetting.

--- core/config/settings.py
+++ core/config/settings.py
@@ -1,16 +1,15 @@
-from langchain_openrouter import ChatOpenRouter
 from dotenv import load_dotenv
-from supabase import create_client, Client
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
-import os
+
+from server.config import settings
 
 load_dotenv()
 
 MAIN_AI_MODEL = "gemini-3.5-flash"
-SUPABASE_KEY=os.getenv("SUPABASE_KEY")
-SUPABASE_SERVICE_ROLE_KEY=os.getenv("SUPABASE_SERVICE_ROLE_KEY")
-SUPABASE_URL=os.getenv("SUPABASE_URL")
+SUPABASE_KEY=settings.SUPABASE_KEY
+SUPABASE_SERVICE_ROLE_KEY=settings.SUPABASE_SERVICE_ROLE_KEY
+SUPABASE_URL=settings.SUPABASE_URL
 
 
 ALTERNATE_GOOGLE_AI_MODELS = ["gemini-3.1-flash-lite", "gemini-2.5-flash"]
@@ -20,7 +19,7 @@ ALTERNATE_GROQ_MODELS = ["llama-3.3-70b-versatile", "openai/gpt-oss-120b"]
 model_list = [ChatGoogleGenerativeAI(
     temperature=0.1,
     model=model_name,
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    google_api_key=settings.GOOGLE_API_KEY,
     max_retries=2,
     thinking_level="medium"
 ) for model_name in ALTERNATE_GOOGLE_AI_MODELS]
@@ -28,7 +27,7 @@ model_list = [ChatGoogleGenerativeAI(
 
 model_list.extend([
     ChatGroq(
-        api_key=os.getenv("GROQ_API_KEY"),
+        api_key=settings.GROQ_API_KEY,
         model=model_name,
         max_retries=2,
         temperature=0.1,
@@ -42,16 +41,12 @@ model_list.extend([
 generator_model = ChatGoogleGenerativeAI(
     temperature=0.1,
     model=MAIN_AI_MODEL,
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    google_api_key=settings.GOOGLE_API_KEY,
     max_retries=2,
     thinking_level="medium"
 )
 
 generator_model = generator_model.with_fallbacks(model_list)
-
-
-
-
 
 
 
---
+++ server/config.py
@@ -0,0 +1,50 @@
+from typing import Optional
+from pydantic_settings import  BaseSettings, SettingsConfigDict
+
+model_config = SettingsConfigDict(
+    env_file=".env",
+    env_file_encoding="utf-8",
+    extra="ignore"
+)
+
+
+class Settings(BaseSettings):
+    app_name: str = "Quick Paper AI"
+
+    # Security
+    SECRET_KEY: str = None
+    ALGORITHM : str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES : int = 60 * 24 * 7
+
+    #Database
+    SUPABASE_KEY: Optional[str] = None
+    SUPABASE_URL : str = ""
+    SUPABASE_SERVICE_ROLE_KEY : str = ""
+    DB_URI : str = ""
+
+
+    #LLM
+    GOOGLE_API_KEY: Optional[str] = None
+    GROQ_API_KEY : Optional[str] = None
+    OPENROUTER_API_KEY : Optional[str] = None
+
+    #Redis
+    UPSTASH_REDIS_REST_URL: Optional[str] = None
+    UPSTASH_REDIS_REST_TOKEN: Optional[str] = None
+
+    # --- Email SMTP (FastMail) ---
+    MAIL_USERNAME: Optional[str] = None
+    MAIL_PASSWORD: Optional[str] = None
+    MAIL_FROM: Optional[str] = None
+    MAIL_PORT: int = 587
+    MAIL_SERVER: str = "smtp.gmail.com"
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+
+settings = Settings()
